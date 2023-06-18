f4f19351b31a02eeea0d5adad4e002141abc5450
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-06-18 22:39:20 +0900

    [design] 회원가입 페이지 화면 UI 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div>
-      <h1>app</h1>
       <Outlet />
     </div>
   );
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,3 +1,66 @@
+import { useState } from "react";
+import { AuthData } from "../../types/authType";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  const [userInfo, setUserInfo] = useState<AuthData>({
+    email: "",
+    password: "",
+  });
+
+  const handleUserInfo = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event?.target;
+    setUserInfo((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen text-center">
+      <form
+        onSubmit={handleSignUp}
+        className={`p-10 border-2 border-black rounded-lg space-y-4`}
+      >
+        <h1 className="text-lg font-bold mb-5">회원가입</h1>
+        <div className="flex justify-between items-center">
+          <label htmlFor="email">이메일 </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={userInfo.email}
+            onChange={handleUserInfo}
+            data-testid="email-input"
+            placeholder="이메일을 입력해주세요."
+            className="border border-black rounded-md px-3 py-1"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">비밀번호 </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={userInfo.password}
+            onChange={handleUserInfo}
+            data-testid="password-input"
+            placeholder="비밀번호를 입력해주세요."
+            className="border border-black rounded-md px-3 py-1"
+          />
+        </div>
+        <button
+          className={`bg-black hover:bg-gray-700 text-white px-10 py-2 rounded-md transition-all duration-300
+        `}
+          data-testid="signup-button"
+          type="submit"
+        >
+          회원가입
+        </button>
+      </form>
+    </div>
+  );
 }
