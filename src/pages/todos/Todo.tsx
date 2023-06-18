618d5b60e129856cc738dbac371bbef900affab3
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-06-19 01:37:51 +0900

    [feat] getTodos api를 통해 투두리스트 목록 불러오기

--- src/pages/todos/Todo.tsx
+++ src/pages/todos/Todo.tsx
@@ -4,19 +4,12 @@ import TodoList from "./components/TodoList";
 import CreateTodo from "./components/CreateTodo";
 import { AuthContext } from "../../context/authContext";
 import { useNavigate } from "react-router-dom";
+import { getTodosApi } from "../../service/todoAPI";
 
 export default function Todo() {
   const { isAuthenticated } = useContext(AuthContext);
 
   const navigate = useNavigate();
-
-  // Assignment 4
-  // 로컬스토리지에 토큰이 없는 상태에 투두리스트 페이지 접속 시 로그인 페이지로 리다이렉트
-  useEffect(() => {
-    if (!isAuthenticated) {
-      navigate("/signin");
-    }
-  });
 
   const [todos, setTodos] = useState<TodoProps[]>([
     {
@@ -50,6 +43,31 @@ export default function Todo() {
       userId: 55,
     },
   ]);
+
+  const getTodos = async () => {
+    try {
+      const response = await getTodosApi();
+      if (response.status !== 200) {
+        return;
+      }
+      setTodos(response.data);
+    } catch (error: any) {
+      alert("오류가 발생했습니다.");
+      console.log(error.response.data.message);
+    }
+  };
+
+  // Assignment 4
+  // 로컬스토리지에 토큰이 없는 상태에 투두리스트 페이지 접속 시 로그인 페이지로 리다이렉트
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/signin");
+    }
+  });
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen text-center">
--- src/pages/todos/components/TodoList.tsx
+++ src/pages/todos/components/TodoList.tsx
@@ -5,9 +5,21 @@ export default function TodoList({ todos }: TodoListProps) {
   return (
     <div className="mt-4">
       <ul className="mt-5">
-        {todos.map((todo: TodoProps) => (
-          <TodoItem key={todo.id} todo={todo.todo} />
-        ))}
+        {todos.length > 0 ? (
+          todos.map((todo: TodoProps) => (
+            <TodoItem key={todo.id} todo={todo.todo} />
+          ))
+        ) : (
+          <li className="text-center h-[200px] flex justify-center items-center">
+            <div>
+              <p className="text-gray-400">할일이 없습니다.</p>
+              <p className="font-medium">
+                새로운 할일을 <span className="text-red-500">추가</span>
+                해주세요!
+              </p>
+            </div>
+          </li>
+        )}
       </ul>
     </div>
   );
