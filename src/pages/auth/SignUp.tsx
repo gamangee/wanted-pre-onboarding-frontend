1dbe72d5bbc4346600b8704951c8aee72ff1508d
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-06-18 23:51:30 +0900

    [feat] 회원가입 페이지 과제 1,2 수행 과제1. 이메일과 비밀번호 유효성 검사에 통과하면 회원가입 버튼이 활성화 되도록 구현 과제2. 정상적으로 회원 가입이 완료되면 로그인 페이지로 이동

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthData } from "../../types/authType";
+import { useNavigate } from "react-router-dom";
+import { signUpApi } from "../../service/authAPI";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+
   const [userInfo, setUserInfo] = useState<AuthData>({
     email: "",
     password: "",
   });
+
+  const [isValid, setIsValid] = useState<boolean>(false);
 
   const handleUserInfo = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event?.target;
@@ -15,15 +21,43 @@ export default function SignUp() {
     }));
   };
 
+  // Assignment 1
+  // 이메일, 비밀번호 유효성 검사 통과에 따른 버튼 활성화 여부
+  useEffect(() => {
+    if (userInfo.email.includes("@") && userInfo.password.length >= 8) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [userInfo]);
+
   const handleSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    try {
+      const response = await signUpApi(userInfo);
+
+      if (response.status !== 201) {
+        alert("회원가입에 실패하였습니다.");
+      }
+
+      alert("회원가입을 성공하였습니다.");
+
+      // Assignment 2
+      // 회원가입 정상 완료시 로그인 페이지로 이동
+      navigate("/signin");
+    } catch (error: any) {
+      alert(`오류가 발생했습니다.\n다시 시도해주세요.`);
+      console.log(error.response.data.message);
+    }
   };
 
   return (
     <div className="flex justify-center items-center h-screen text-center">
       <form
         onSubmit={handleSignUp}
-        className={`p-10 border-2 border-black rounded-lg space-y-4`}
+        className={`p-10 border-2 rounded-lg space-y-4 ${
+          isValid ? "border-blue-500" : "border-black"
+        }`}
       >
         <h1 className="text-lg font-bold mb-5">회원가입</h1>
         <div className="flex justify-between items-center">
@@ -53,8 +87,10 @@ export default function SignUp() {
           />
         </div>
         <button
-          className={`bg-black hover:bg-gray-700 text-white px-10 py-2 rounded-md transition-all duration-300
-        `}
+          className={` text-white px-10 py-2 rounded-md transition-all duration-300 ${
+            isValid ? "bg-blue-500" : "bg-black"
+          }`}
+          disabled={!isValid}
           data-testid="signup-button"
           type="submit"
         >
