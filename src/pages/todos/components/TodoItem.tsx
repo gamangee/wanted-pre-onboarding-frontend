2c40efd2c57701e95e3f9f21963cc45388490030
MoonJung Jang <84329979+gamangee@users.noreply.github.com>
2023-06-19 02:40:14 +0900

    [feat] 투두리스트 페이지 과제 8,10 수행 1. TODO 수정, 삭제 버튼 구현 2. 수정 기능 구현 - 수정모드에서만 TODO 내용 변경 가능 - 취소버튼을 누르면 수정한 내용 초기화, 수정 모드 비활성화 - 제출버튼을 누르면 수정한 내용 업데이트

--- src/pages/todos/components/TodoItem.tsx
+++ src/pages/todos/components/TodoItem.tsx
@@ -1,5 +1,6 @@
-import { deleteTodoApi } from "../../../service/todoAPI";
-import { TodoItemProps } from "../../../types/todoType";
+import { useState } from "react";
+import { deleteTodoApi, updateTodoApi } from "../../../service/todoAPI";
+import { TodoItemProps, UpdateTodoProps } from "../../../types/todoType";
 
 export default function TodoItem({
   id,
@@ -7,6 +8,84 @@ export default function TodoItem({
   isCompleted,
   setTodos,
 }: TodoItemProps) {
+  // 수정 모드
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+
+  // 수정한 투두 업데이트
+  const [updatedTodo, setUpdatedTodo] = useState<string>(todo);
+
+  // 수정 모드 활성화
+  const handleEdit = () => {
+    setIsEditMode(true);
+  };
+
+  // 수정 모드 취소
+  const handleCancel = () => {
+    // 수정 모드 비활성화
+    setIsEditMode(false);
+    // 수정 내용 초기화
+    setUpdatedTodo(todo);
+  };
+
+  // 수정 입력
+  const onChangeTodo = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUpdatedTodo(event.target.value);
+  };
+
+  // Assignment 7
+  // 체크박스를 통해 할일 완료 여부 수정
+  const handleCheckboxChange = async () => {
+    try {
+      const updateTodoState: UpdateTodoProps = {
+        todo,
+        isCompleted: !isCompleted,
+      };
+      const response = await updateTodoApi(id, updateTodoState);
+      if (response.status !== 200) {
+        alert("체크 변경을 실패했습니다.");
+        return;
+      }
+      setTodos((todos) =>
+        todos.map((todo) =>
+          todo.id === response.data.id ? response.data : todo
+        )
+      );
+    } catch (error: any) {
+      alert(`오류가 발생했습니다.\n다시 시도해주세요.`);
+      console.log(error.response.data.message);
+    }
+  };
+
+  // Assignment 10 수정 기능
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      const updateTodoText: UpdateTodoProps = {
+        todo: updatedTodo,
+        isCompleted,
+      };
+
+      const response = await updateTodoApi(id, updateTodoText);
+
+      if (response.status !== 200) {
+        alert("수정하는데 실패했습니다.");
+        return;
+      }
+
+      setTodos((todos) =>
+        todos.map((todo) =>
+          todo.id === response.data.id ? response.data : todo
+        )
+      );
+
+      setIsEditMode(false);
+    } catch (error: any) {
+      alert(`오류가 발생했습니다.\n다시 시도해주세요.`);
+      console.log(error.response.data.message);
+    }
+  };
+
+  // Assignment 9 삭제 기능
   const handleDelete = async () => {
     try {
       const response = await deleteTodoApi(id);
@@ -26,35 +105,67 @@ export default function TodoItem({
 
   return (
     <li className="p-4 border-b last:border-none">
-      {/* Assignment 5 */}
-      {/* li,label,input,span 태그 사용 */}
-      <div className="flex justify-between items-center space-x-2">
-        <div>
-          <label className="flex items-center space-x-1 cursor-pointer">
-            {/* Assignment 7 */}
-            {/* 체크박스를 통해 할일 완료 여부 수정 */}
-            <input type="checkbox" checked={isCompleted} />
-            <span className={isCompleted ? "line-through" : ""}>{todo}</span>
-          </label>
+      {isEditMode ? (
+        <form
+          className="flex justify-between items-center space-x-2"
+          onSubmit={handleSubmit}
+        >
+          <div className="w-4/5">
+            <input
+              data-testid="modify-input"
+              type="text"
+              value={updatedTodo}
+              onChange={onChangeTodo}
+              className="rounded border border-green-500 px-4 py-1 w-full focus:outline-green-500"
+            />
+          </div>
+          <div className="w-1/5">
+            <button
+              data-testid="submit-button"
+              type="submit"
+              className="mr-2 bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded"
+            >
+              제출
+            </button>
+            <button
+              data-testid="cancel-button"
+              onClick={handleCancel}
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
+            >
+              취소
+            </button>
+          </div>
+        </form>
+      ) : (
+        <div className="flex justify-between items-center space-x-2">
+          <div className="w-4/5">
+            <label className="flex items-center space-x-1 cursor-pointer">
+              <input
+                type="checkbox"
+                checked={isCompleted}
+                onChange={handleCheckboxChange}
+              />
+              <span className={isCompleted ? "line-through" : ""}>{todo}</span>
+            </label>
+          </div>
+          <div className="w-1/5">
+            <button
+              onClick={handleEdit}
+              data-testid="modify-button"
+              className="mr-2 bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-1 px-2 rounded"
+            >
+              수정
+            </button>
+            <button
+              onClick={handleDelete}
+              data-testid="delete-button"
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
+            >
+              삭제
+            </button>
+          </div>
         </div>
-        <div>
-          {/* Assignment 8 */}
-          {/* 수정, 삭제 버튼 속성 부여 */}
-          <button
-            data-testid="modify-button"
-            className="mr-2 bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-1 px-2 rounded"
-          >
-            수정
-          </button>
-          <button
-            onClick={handleDelete}
-            data-testid="delete-button"
-            className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
-          >
-            삭제
-          </button>
-        </div>
-      </div>
+      )}
     </li>
   );
 }
